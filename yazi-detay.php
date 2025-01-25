04d02cc694d94cbd07b6feae92fb32bd3386016b
ZaeemRizwan2003 <210657@students.au.edu.pk>
2025-01-25 16:36:58 +0500

    new

--- yazi-detay.php
+++ yazi-detay.php
@@ -2,12 +2,18 @@
 <?php require_once 'req/head_start.php'; ?>
 
 <?php
-$link = g('link');
-$textID = g('id');
-$detail = $db->get_row("SELECT * FROM the_blog WHERE slug = '$link' AND blog_id = '$textID'");
+$link = g('link') ?? ''; 
+$textID = g('id') ?? ''; 
+
+// Using prepared statements to prevent SQL injection
+$detail = $db->get_row("SELECT * FROM the_blog WHERE slug = ? AND blog_id = ?", [$link, $textID]);
+
+if (!$detail) {
+    die("Blog post not found.");
+}
 ?>
 
-<title><?=$detail->name?></title>
+<title><?= htmlspecialchars($detail->name ?? 'Blog Post') ?></title>
 <?php require_once 'req/head.php'; ?>
     <!-- SPECIFIC CSS -->
     <link href="lib/css/blog.css" rel="stylesheet">
@@ -19,7 +25,7 @@ $detail = $db->get_row("SELECT * FROM the_blog WHERE slug = '$link' AND blog_id 
         <section class="hero_in general">
             <div class="wrapper">
                 <div class="container">
-                    <h1 class="fadeInUp"><span></span> blog</h1>
+                    <h1 class="fadeInUp"><span></span> Blog</h1>
                 </div>
             </div>
         </section>
@@ -29,20 +35,31 @@ $detail = $db->get_row("SELECT * FROM the_blog WHERE slug = '$link' AND blog_id 
             <div class="row">
                 <div class="col-lg-9">
                     <div class="bloglist singlepost">
-                        <?php if($detail->picture){ ?>
-                        <p><img alt="" class="img-fluid" src="data/blog/<?=$detail->picture?>"></p>
+                        <?php if (!empty($detail->picture)) { ?>
+                        <p><img alt="" class="img-fluid" src="data/blog/<?= htmlspecialchars($detail->picture) ?>"></p>
                         <?php } ?>
-                        <h1><?=$detail->name?></h1>
+                        <h1><?= htmlspecialchars($detail->name) ?></h1>
                         <div class="postmeta">
-                            <ul><?php $katBuls = $db->get_row("SELECT * FROM the_blog_category WHERE category_id = '$detail->blog_category_id'"); ?>
-                                <li><a href="blog/kategori/<?=$katBuls->slug?>/<?=$katBuls->category_id?>"><i class="icon_folder-alt"></i> <?=$katBuls->name?></a></li>
-                                <li><a href="#"><i class="icon_clock_alt"></i> <?=timeTR($detail->created_at)?></a></li>
+                            <ul>
+                                <?php 
+                                $katBuls = $db->get_row("SELECT * FROM the_blog_category WHERE category_id = ?", [$detail->blog_category_id]); 
+                                if ($katBuls): 
+                                ?>
+                                    <li>
+                                        <a href="blog/kategori/<?= htmlspecialchars($katBuls->slug) ?>/<?= htmlspecialchars($katBuls->category_id) ?>">
+                                            <i class="icon_folder-alt"></i> <?= htmlspecialchars($katBuls->name) ?>
+                                        </a>
+                                    </li>
+                                <?php endif; ?>
+                                <li>
+                                    <a href="#"><i class="icon_clock_alt"></i> <?= timeTR($detail->created_at) ?></a>
+                                </li>
                             </ul>
                         </div>
                         <!-- /post meta -->
                         <div class="post-content">
                             <div class="dropcaps">
-                                <?=$detail->content?>
+                                <?= $detail->content ?? '' ?>
                             </div>
                             <div class="clearfix"></div>
                         </div>
@@ -58,7 +75,7 @@ $detail = $db->get_row("SELECT * FROM the_blog WHERE slug = '$link' AND blog_id 
                 <!-- /col -->
 
                 <aside class="col-lg-3">
-                <?php include 'inc/blog.side.php'; ?>
+                    <?php include 'inc/blog.side.php'; ?>
                 </aside>
                 <!-- /aside -->
             </div>
@@ -70,6 +87,4 @@ $detail = $db->get_row("SELECT * FROM the_blog WHERE slug = '$link' AND blog_id 
 
 <?php require_once 'req/footer.php'; ?>
 <?php require_once 'req/script.php'; ?>
-
-
 <?php require_once 'req/body_end.php'; ?>
